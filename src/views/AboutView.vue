52cb91276a6216ec38ecbd167e70e94318831d21
YuuuFen <yufenhuangtaiwan@gmail.com>
2024-01-15 00:08:43 +0800

    'works for me' version

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,22 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <a
+      href="https://happenedinnz.com/apply-working-holiday-visa/"
+      target="_blank"
+      >2023線上申請打工渡假簽證教學＆注意事項</a
+    >
+    <br />
+    <a
+      href="https://www.immigration.govt.nz/new-zealand-visas/visas/visa/taiwan-working-holiday-visa"
+      target="_blank"
+      >Taiwan Working Holiday Visa</a
+    >
+    <br />
+    <a
+      href="https://onlineservices.immigration.govt.nz/WorkingHoliday/"
+      target="_blank"
+      >Working Holiday Visa Application Forms</a
+    >
   </div>
 </template>
--- src/views/IndexView.vue
+++ src/views/IndexView.vue
@@ -1,8 +1,62 @@
 <script setup>
+import { ref } from "vue";
+import data from "@/assets/data.json";
+
+const copiedData = ref(null);
+
+function copyToClipboard(value) {
+  navigator.clipboard.writeText(value);
+  copiedData.value = value;
+}
+
+function getFirstLetter(value) {
+  if (value.charAt(0) === "(") {
+    return true;
+  }
+  return false;
+}
 </script>
 
 <template>
-  <div class="index">
-    <h1>This is an index page</h1>
+  <div v-if="!!copiedData" class="toast-result">
+    Copied the text: {{ copiedData }}
   </div>
+  <div
+    v-for="(title, titleName) in data"
+    :key="titleName"
+    style="padding: 0 1rem"
+  >
+    <h2 style="margin: 2px">{{ titleName }}</h2>
+    <div v-for="(subTitle, subTitleName) in title" :key="subTitleName">
+      <h3 style="margin: 2px">{{ subTitleName }}</h3>
+      <div v-for="(lineTitle, lineTitleName) in subTitle" :key="lineTitleName">
+        <div
+          v-for="(trueTitle, trueTitleName) in lineTitle"
+          :key="trueTitleName"
+          style="margin: 0 0 2px 1rem"
+        >
+          {{ trueTitleName }}
+          <button
+            type="button"
+            @click="copyToClipboard(trueTitle)"
+            :disabled="getFirstLetter(trueTitle)"
+          >
+            {{ trueTitle }}
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- <div>{{ data }}</div> -->
 </template>
+
+<style scoped>
+.toast-result {
+  position: fixed;
+  top: 4rem;
+  right: 2rem;
+  background-color: rgb(255, 199, 150);
+  padding: 10px;
+  border: 1px solid #000;
+}
+</style>
